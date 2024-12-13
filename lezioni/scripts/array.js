dbc4a8bd760787b5fc5909237fe77208e831c29d
andreatrentini <55395467+andreatrentini@users.noreply.github.com>
2024-12-13 08:30:47 +0000

    filter e Map

--- lezioni/scripts/array.js
+++ lezioni/scripts/array.js
@@ -1,26 +1,34 @@
 var dati;
 
-async function getAeroporti(funzioneDiCallback, criterio) {
+async function getAeroporti() {
     try {
         const risposta = await fetch('../data/aeroporti.json');
         // Salvo i dati nella variabile GLOBALE dati
         dati = await risposta.json();
-        funzioneDiCallback(dati, criterio)
+        const tabellaDati = document.getElementById('tabella-dati');
+        tabellaDati.innerText = '';
+        tabellaDati.appendChild(createTable(dati));
     }
     catch (error) {
     }
 }
 
 function filterAeroporti(aeroporti, criterio) {
-   /*  let aeroportiFiltrati = [];
+    /*  let aeroportiFiltrati = [];
+ 
+     for(let i=0; i < aeroporti.length; i++) {
+         if (aeroporti[i].name.includes(criterio)) {
+             aeroportiFiltrati.push(aeroporti[i])
+         }
+     } */
 
-    for(let i=0; i < aeroporti.length; i++) {
-        if (aeroporti[i].name.includes(criterio)) {
-            aeroportiFiltrati.push(aeroporti[i])
-        }
-    } */
-
-        let aeroportiFiltrati = aeroporti.filter(aeroporto => aeroporto.name.startsWith(criterio));
+    // Ottengo un array con i soli aeroporti che soddisfano i criteri di ricerca (iniziano per ...)
+    let aeroportiFiltrati = aeroporti.filter(aeroporto => aeroporto.name.toUpperCase().startsWith(criterio.toUpperCase()));
+    
+    // Rigenero la tabella con gli aeroporti, usando non dati (che contiene l'inero elenco), ma aeroportiFiltrati
+    const tabellaDati = document.getElementById('tabella-dati');
+    tabellaDati.innerText = '';
+    tabellaDati.appendChild(createTable(aeroportiFiltrati));
 }
 
 function createTable(aeroporti) {
@@ -57,6 +65,26 @@ function createTableRow(aeroporto) {
     return row;
 }
 
-getAeroporti(() => {
-    let aeroportiFiltrati = aeroporti.filter(aeroporto => aeroporto.name.startsWith(criterio));
-}, 'A');
+function cambiaVisualizzazione() {
+    const nuovaVisualizzazione = dati.map(aeroporto => {
+        return {
+            name: aeroporto.name, 
+            country_code: aeroporto.country_code}
+    });
+    console.log(nuovaVisualizzazione);
+}
+
+function statistiche() {
+    const country_code = dati.map(aeroporto => aeroporto.country_code);
+    console.log(country_code);
+    const country_code_singoli = new Set(country_code);
+    console.log(country_code_singoli);
+
+}
+
+getAeroporti();
+
+var iniziali = document.getElementById('iniziali');
+iniziali.addEventListener('input', () => {
+    filterAeroporti(dati, iniziali.value);
+})
